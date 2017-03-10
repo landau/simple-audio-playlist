14f2de9a60090b564c031e23461e5c19250ff0a4
Trevor Landau <landautrevor@gmail.com>
2017-03-10 08:47:26 -0500

    import js

--- bin/build.js
+++ bin/build.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 const TEMPLATES_DIR = path.join(__dirname, '..', 'templates');
 const INDEX_HTML = fs.readFileSync(path.join(TEMPLATES_DIR, 'index.html'), 'utf8');
+const JS = fs.readFileSync(path.join(TEMPLATES_DIR, 'index.js'), 'utf8');
 
 const CWD = process.cwd();
 
@@ -13,5 +14,6 @@ const fileNames = fs.readdirSync(CWD).filter(f => /\.mp3$/.test(f));
 // TODO: get time
 const html = INDEX_HTML
   .replace('{data}', JSON.stringify(fileNames))
+  .replace('{js}', JS) 
   .replace('{title}', process.argv[2] || 'Simple Audio Player');
 console.log(html);
